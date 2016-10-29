fb8da126bb99b4f72feecd6b0ebe4bc7b6df8dcb
Dean777 <deanbojinov@abv.bg>
2016-10-29 13:55:52 +0300

    Signed-off-by: Dean777 <deanbojinov@abv.bg>

---
+++ TeamDataForum/TeamDataForum/Models/BindingModels/UserLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamDataForum.Models
+{
+    public class UserLog
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
