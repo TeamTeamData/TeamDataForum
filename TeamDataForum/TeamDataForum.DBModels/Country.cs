2cdb8a60421e119eda871289a07aecbc540d2a15
Emiliyan Evgeniev <mr_e@abv.bg>
2016-10-29 09:55:57 +0300

    Entity models
    Add town model

--- TeamDataForum/TeamDataForum.DBModels/Country.cs
+++ TeamDataForum/TeamDataForum.DBModels/Country.cs
@@ -1,6 +1,7 @@
 ﻿namespace TeamDataForum.DBModels
 {
     using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Country
     {
@@ -9,6 +10,7 @@
 
         [Required]
         [MaxLength(100, ErrorMessage = "Country name is required.")]
+        [Index()]
         public string Name { get; set; }
     }
 }
