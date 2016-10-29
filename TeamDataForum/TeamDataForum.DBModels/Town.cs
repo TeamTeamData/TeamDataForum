037ce21dbb47ce6ab3633877b715649d255ada1f
Emiliyan Evgeniev <mr_e@abv.bg>
2016-10-29 09:56:36 +0300

    Entity models
    Add town model.

---
+++ TeamDataForum/TeamDataForum.DBModels/Town.cs
@@ -0,0 +1,16 @@
+﻿namespace TeamDataForum.DBModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Town
+    {
+        [Key]
+        public int TownId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+}
