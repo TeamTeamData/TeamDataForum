4da8fac02bcb63c92995b4920ef5e4c10f92cd2f
Emiliyan Evgeniev <mr_e@abv.bg>
2016-10-29 09:50:26 +0300

    Entity models
    Add country model.

---
+++ TeamDataForum/TeamDataForum.DBModels/Country.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamDataForum.DBModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Country
+    {
+        [Key]
+        public int CountryID { get; set; }
+
+        [Required]
+        [MaxLength(100, ErrorMessage = "Country name is required.")]
+        public string Name { get; set; }
+    }
+}
