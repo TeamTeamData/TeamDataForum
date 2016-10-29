49cf3e1f90842a9b82fc0a0412980c025244feae
Emiliyan Evgeniev <mr_e@abv.bg>
2016-10-29 10:01:25 +0300

    Entity models
    Add User model

---
+++ TeamDataForum/TeamDataForum.DBModels/User.cs
@@ -0,0 +1,17 @@
+﻿namespace TeamDataForum.DBModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Required]
+        [MaxLength(50, ErrorMessage = "First name is required.")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Last name is required.")]
+        public string Lastname { get; set; }
+
+        public virtual Town Town { get; set; }
+    }
+}
