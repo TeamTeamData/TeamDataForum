7b1686702570739e857eca7a8050d7c8f86d0716
Emiliyan Evgeniev <mr_e@abv.bg>
2016-11-01 09:24:39 +0200

    MVC - controllers
    Add base controller with unit of work.

---
+++ TeamDataForum/TeamDataForum.Web/Controllers/Bases/ForumBaseController.cs
@@ -0,0 +1,36 @@
+﻿namespace TeamDataForum.Web.Controllers.Bases
+{
+    using System;
+    using System.Web.Mvc;
+    using UnitOfWork.Contracts;
+
+    /// <summary>
+    /// Base controller for all Mvc Forum controllers
+    /// </summary>
+    public class ForumBaseController : Controller
+    {
+        private const string UnitNullError = "Unit of work cannot be null.";
+
+        private IUnitOfWork unitOfWork;
+
+        public ForumBaseController(IUnitOfWork unitOfWork)
+        {
+            this.UnitOfWork = unitOfWork;
+        }
+
+        protected IUnitOfWork UnitOfWork
+        {
+            get { return this.unitOfWork; }
+
+            private set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(UnitNullError);
+                }
+
+                this.unitOfWork = value;
+            }
+        }
+    }
+}
