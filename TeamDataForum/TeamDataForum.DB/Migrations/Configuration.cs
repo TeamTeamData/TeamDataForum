b78c6116f30d1d8a5d8c1b627be7e4f11b67bfdf
Emiliyan Evgeniev <mr_e@abv.bg>
2016-11-03 17:06:55 +0200

    DB
    Seeding - Towns model.

--- TeamDataForum/TeamDataForum.DB/Migrations/Configuration.cs
+++ TeamDataForum/TeamDataForum.DB/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace TeamDataForum.DB.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Linq;
+    using DBModels;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeamDataForumContext>
     {
@@ -12,7 +14,26 @@ namespace TeamDataForum.DB.Migrations
 
         protected override void Seed(TeamDataForumContext context)
         {
-            
+            this.CreateTowns(context);
+        }
+
+        private void CreateTowns(TeamDataForumContext context)
+        {
+            string sofia = "Sofia";
+
+            if (!context.Towns.Any(t => t.Name == sofia))
+            {
+                context.Towns.Add(new Town()
+                {
+                    Name = sofia,
+                    Country = new Country()
+                    {
+                        Name = "Bulgaria"
+                    }
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }
