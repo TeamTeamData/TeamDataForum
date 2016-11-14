f1e2e8b5fb80c862d3015c48c11268a890d656ad
Emiliyan Evgeniev <mr_e@abv.bg>
2016-11-14 15:12:17 +0200

    Web
    Removing model.

--- TeamDataForum/TeamDataForum.Web/Models/ViewModels/Threads/ThreadsViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TeamDataForum.Web.Models.ViewModels.Threads
-{
-    using System.Collections.Generic;
-
-    public class ThreadsViewModel
-    {
-        public IEnumerable<ThreadViewModel> Threads { get; set; }
-    }
-}
