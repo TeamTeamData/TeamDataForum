3329e64fc608f2735ece10998d7c2a74027ad65f
Emiliyan Evgeniev <mr_e@abv.bg>
2016-11-17 15:56:41 +0200

    Web
    Create forum.

---
+++ TeamDataForum/TeamDataForum.Web/Controllers/ForumController.cs
@@ -0,0 +1,71 @@
+﻿namespace TeamDataForum.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Bases;
+    using DBModels;
+    using Models.BindingModels.Forums;
+    using UnitOfWork.Contracts;
+
+    public class ForumController : ForumBaseController
+    {
+        public ForumController(IUnitOfWork unitOfWork) 
+            : base(unitOfWork)
+        {
+        }
+
+        // GET: Forum
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Empty create for creating Forum
+        /// </summary>
+        /// <returns>View for creating forum</returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ForumBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            User user = this.UnitOfWork
+                .UserRepository
+                .Select(u => u.UserName == this.User.Identity.Name)
+                .FirstOrDefault();
+
+            Forum newForum = new Forum()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Date = DateTime.Now,
+                Creator = user
+            };
+
+            newForum.Moderators.Add(user);
+
+            newForum = this.UnitOfWork
+                .ForumRepository
+                .Add(newForum);
+
+            this.UnitOfWork
+                .ForumRepository
+                .SaveChanges();
+
+            // to do change
+            return RedirectToAction("Home", "Home", null);
+        }
+    }
+}
---
+++ TeamDataForum/TeamDataForum.Web/Models/BindingModels/Forums/ForumBindingModel.cs
@@ -0,0 +1,19 @@
+﻿namespace TeamDataForum.Web.Models.BindingModels.Forums
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ForumBindingModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Forum title is required.")]
+        [MaxLength(300, ErrorMessage = "Forum title cannot be more than 300 symbols.")]
+        [Display(Name = "Forum title")]
+        [DataType(DataType.MultilineText)]
+        public string Title { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Forum description is required.")]
+        [MaxLength(300, ErrorMessage = "Forum description cannot be more than 300 symbols.")]
+        [Display(Name = "Forum description")]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+    }
+}
