fea211e76542d718c09c94328c69ba978ec07bf4
Emiliyan Evgeniev <mr_e@abv.bg>
2016-12-10 10:58:25 +0200

    Revert "Web"
    This reverts commit 277f1c4404f8787a499819a2186e7bbd04c9513c.

--- TeamDataForum/TeamDataForum.Web/App_Start/NinjectWebCommon.cs
+++ TeamDataForum/TeamDataForum.Web/App_Start/NinjectWebCommon.cs
@@ -5,26 +5,30 @@ namespace TeamDataForum.Web.App_Start
 {
     using System;
     using System.Web;
-
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
+    using Controllers.Bases;
+    using DB;
+    using Pagination;
+    using Pagination.Contracts;
+    using UnitOfWork;
+    using UnitOfWork.Contracts;
 
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -32,7 +36,7 @@ namespace TeamDataForum.Web.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -61,6 +65,10 @@ namespace TeamDataForum.Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+            kernel.Bind<TeamDataForumContext>().To<TeamDataForumContext>();
+            kernel.Bind<IPaginationFactory>().To<PaginationFactory>();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            kernel.Bind<ForumBaseController>().To<ForumBaseController>();
+        }
     }
 }
